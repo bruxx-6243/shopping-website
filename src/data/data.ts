37adb7d75d1f9f830aba96b5594fa17cfc8cb750
Fariol Blondeau <blondeau.nbif@mail.ru>
2023-05-25 19:33:22 +0300

    fix error on production mode

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,26 +5,26 @@ export interface IProduct {
 
 export const newArrivals: IProduct[] = [
   {
-    title: "Hoodies & Sweetshirt",
-    image: "images/1.png",
+    title: "Hoodies & Sweatshirt",
+    image: "1.png",
   },
   {
     title: "Coats & Parkas",
-    image: "images/2.png",
+    image: "2.png",
   },
   {
     title: "Tees & T-Shirt",
-    image: "images/3.png",
+    image: "3.png",
   },
 ];
 
 export const favorites: IProduct[] = [
   {
     title: "Trending on instagram",
-    image: "images/4.png",
+    image: "4.png",
   },
   {
     title: "All Under $40",
-    image: "images/5.png",
+    image: "5.png",
   },
 ];
